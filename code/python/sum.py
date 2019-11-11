94e8bd4e10c5c0f7ae10278bcef053b6a78f436e
taeyoungchoon <taeyoungchoon@gmail.com>
2019-11-11 23:12:44 +0900

    d

---
+++ code/python/sum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+max = 100
+
+"""
+sum of number
+and slow
+"""
+
+sum = 0
+for num in range(1, max + 1):
+    sum = sum + num
+
+print(type(sum))
+print(sum)
+
+"""
+cons half
+and fast
+"""
+
+sum = 0
+half = max / 2
+sum = str(int(half)) + str(int(half))
+print(type(sum))
+print(sum)
